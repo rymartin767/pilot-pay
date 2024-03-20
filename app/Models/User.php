769068c7316a910eac70bede6f22622c75637a79
Ryan Martin <ryan@nt4c.com>
2024-03-20 08:34:49 -0400

    filament install + initial test

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,20 +3,28 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Panel;
+use Laravel\Sanctum\HasApiTokens;
+use Laravel\Jetstream\HasProfilePhoto;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens;
     use HasFactory;
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->hasVerifiedEmail();
+    }
 
     /**
      * The attributes that are mass assignable.
---
+++ tests/Feature/FilamentDashboardTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Models\User;
+use function Pest\Laravel\actingAs;
+
+test('filament dashboard is not available to guest', function () {
+    $this->get('/dashboard')
+        ->assertStatus(302)
+        ->assertRedirect('/dashboard/login');
+});
+
+test('filament dashboard is available to auth users', function () {
+    actingAs(User::factory()->create())
+        ->get('/dashboard')
+        ->assertStatus(200);
+});
